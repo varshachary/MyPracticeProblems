c0889d41b3c839ed2fd1f79909252f3af9f300a8
Varsha Chary <34280450+varshachary@users.noreply.github.com>
2021-05-17 12:23:59 +0530

    Create morse_code.py

---
+++ morse_code.py
@@ -0,0 +1,36 @@
+"""
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+#leetcode
+"""
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse_code=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        result=set()
+        for i in range(0,len(words)):
+            s=""
+            for x in words[i]:
+                s=s+morse_code[ord(x)-ord('a')]
+            result.add(s)
+        return len(result)
+            
+        
+"""
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+"""
