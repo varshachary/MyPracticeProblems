afec4b9d4f15a3b46ca6d89823b6880ad514ffb6
Varsha Chary <34280450+varshachary@users.noreply.github.com>
2021-05-20 12:03:55 +0530

    Create greatest_number_of_candies.py

---
+++ greatest_number_of_candies.py
@@ -0,0 +1,29 @@
+"""
+Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+
+For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
+
+leetcode
+"""
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_cand=max(candies)
+        result=[]
+        for i in candies:
+            if(i+extraCandies >= max_cand):
+                result.append(True)
+            else:
+                result.append(False)
+        return result
+            
+   
+"""
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: 
+Kid 1 has 2 candies and if he or she receives all extra candies (3) will have 5 candies --- the greatest number of candies among the kids. 
+Kid 2 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids. 
+Kid 3 has 5 candies and this is already the greatest number of candies among the kids. 
+Kid 4 has 1 candy and even if he or she receives all extra candies will only have 4 candies. 
+Kid 5 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number of candies among the kids. 
+"""
