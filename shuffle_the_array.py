3c896ace75482c877f165ec85949fbead81daf3a
Varsha Chary <34280450+varshachary@users.noreply.github.com>
2021-05-24 09:53:22 +0530

    Create shuffle_the_array.py

---
+++ shuffle_the_array.py
@@ -0,0 +1,22 @@
+"""
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+#leetcode
+"""
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result=[]
+        for i in range(0,n):
+            result.append(nums[i])
+            result.append(nums[i+n])
+        return result
+            
+ 
+"""
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+"""
